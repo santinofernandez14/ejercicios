e53f08b2d8df7af90d4b3cce509ad5728e7df7c1
ariel7171 <141683972+ariel7171@users.noreply.github.com>
2024-02-26 20:58:43 -0300

    asd

--- Parte B/05/app.js
+++ Parte B/05/app.js
@@ -1,15 +1,17 @@
+// 5) Realice una página donde el usuario pueda cargar un contacto. El contacto de esa
+// persona debe tener al menos un teléfono y como máximo 3. Además debe ser obligatoria la
+// carga del mail. Realice las validaciones necesarias.
+
 function validar() {
         
     let miFormulario = document.forms["formulario"]
     let gmail2 = miFormulario.gmail.value; 
     let numero = miFormulario.numero.value;
+    let patronEmail= /^\S+@\S+\.\S+$/;
 
-    
     console.log("Este es su mail :" + gmail2)
-   
- 
     
-    if (gmail2 == "" || isNaN(numero) || numero == "") {
+    if (patronEmail.test(gmail2) || gmail2=="" || isNaN(numero) || numero == "") {
         alert("Ingrese bien los campos")
         return true;
     } else {
@@ -17,6 +19,5 @@ function validar() {
         alert("Su usuario se ingreso con exito")
         return false;
     }
-
 
 }
