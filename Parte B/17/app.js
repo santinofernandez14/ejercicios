f579500f2ca55da3c6140ad4212ede29a8c9533e
ariel7171 <141683972+ariel7171@users.noreply.github.com>
2024-02-26 20:15:26 -0300

    17

--- Parte B/17/app.js
+++ Parte B/17/app.js
@@ -1,10 +1,13 @@
-function esperar(){
-    let caja=document.getElementById("box")
-    caja.disabled=true;
-    setTimeout(function() {
+// 17) Realice una página que escriba en un campo de texto una cadena. La cadena debe ser
+// escrita de a una letra por vez y el tiempo entre escritura de la letra debe ser 700ms.
+
+function esperar() {
+
+    let caja = document.getElementById("box")
+    caja.disabled = true;
+    setTimeout(function () {
         caja.disabled = false;
+
     }, 700);
     
-    
-   
 }
